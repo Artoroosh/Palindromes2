f57df6ae97b0909e7efb7802bdef229bb6dc26df
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-09-11 15:06:28 -0400

    commit2

--- src/palindrome2/Palindrome2.java
+++ src/palindrome2/Palindrome2.java
@@ -13,15 +13,17 @@ import javax.swing.JOptionPane;
      String sentence = JOptionPane.showInputDialog
       ("Words that are arranged the same way forwards as they are backwards are called palindromes"
       + "\n\nEnter a Word");
-     
+    String word = null;
     String sentence2 = sentence.toLowerCase();
     while(sentence2.length()>0){
     int spacePos = sentence.indexOf(" ");
-    String word = sentence.substring(0,spacePos);
+    word = sentence.substring(0,spacePos);
     String Sentence = sentence.substring(spacePos + 1,sentence.length());
        
   }
- 
+ String backwardsWord = "";
+for (int pos = word.length() - 1; pos >= 0; pos--) {
+backwardsWord += word.charAt(pos);
     
     
     
